0183e63c87576f6e1df31b94f97a561a5d0aa009
Gimli Kim <infinite1st@gmail.com>
2024-04-17 18:54:33 +0900

    Initial Commit

---
+++ babystudy_memo/babystudy_memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  babystudy_memoApp.swift
+//  babystudy_memo
+//
+//  Created by kyunglimkim on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct babystudy_memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
